1809cec3ec60752033d6627a298cc4eac1cf47da
PolinaSloboda <sloboda.polina@gmail.com>
2019-04-26 15:06:55 +0300

    поправила форматирование

--- script.js
+++ script.js
@@ -1,16 +1,16 @@
 new Vue({
-  el: '#app',
-	data () {
-    return {
-      modalHidden: true
+    el: '#app',
+    data () {
+        return {
+          modalHidden: true
+        }
+    },
+    methods: {
+    	showFeedback () {
+    		this.modalHidden = false;
+    	},
+        closeFeedback () {
+          this.modalHidden = true;
+        }
     }
-  },
-	methods: {
-		showFeedback () {
-			this.modalHidden = false;
-		},
-    closeFeedback () {
-      this.modalHidden = true;
-    }
-	}
 })
